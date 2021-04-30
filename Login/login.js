5a77c5103d4e66f87cd8a7a566dbc7bf625ca94e
RoSA2021 <79340520+SaulAI2021@users.noreply.github.com>
2021-04-29 22:31:10 -0500

    LogiGridResponsive

---
+++ Login/login.js
@@ -0,0 +1,28 @@
+/*codigo js j&g*/
+let login= document.querySelector(".login");
+let singup=document.querySelector(".singup");
+
+const btnR = document.querySelector("#btnR");
+const btnL = document.querySelector("#btnL");
+
+
+singup.style.transition="all 0.5s"
+login.style.transition = "all 0.5s";
+
+function next() {
+  login.style.marginLeft = "-100%";
+  singup.style.marginLeft="-100%";
+}
+function prev() {
+    login.style.marginLeft="0%";
+    singup.style.marginLeft="0%";
+}
+
+btnL.addEventListener("click", function () {
+    prev();
+  });
+  
+btnR.addEventListener("click", function () {
+    next();
+  });
+
