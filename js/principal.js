0a4e868bfae77d87422da54858d80b9f54920e7c
Nayelhy Renteria <59325107+NayelhyRC@users.noreply.github.com>
2021-05-08 02:02:49 -0500

    F, borré lo del menú

--- js/principal.js
+++ js/principal.js
@@ -1,11 +1,9 @@
-function ocultarMenu() {
-  var elemento = document.getElementById("listaMenu");
-  elemento.style.display = "none";
-  // var elemento = document.getElementById("menu-icon");
-  // elemento.style.content = "\2261";
-}
+// function ocultarMenu() {
+//   var elemento = document.getElementById("listaMenu");
+//   elemento.style.display = "none";
+// }
 
-function mostrarMenu() {
-  var elemento = document.getElementById("listaMenu");
-  elemento.style.display = "initial";
-}
+// function mostrarMenu() {
+//   var elemento = document.getElementById("listaMenu");
+//   elemento.style.display = "initial";
+// }
