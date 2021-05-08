e0c8369a2f548c707d0f8e61d49c453722f1b5f4
Nayelhy Renteria <59325107+NayelhyRC@users.noreply.github.com>
2021-05-08 01:59:05 -0500

    desaparece el menú pero no lo hace bien :v

---
+++ js/principal.js
@@ -0,0 +1,11 @@
+function ocultarMenu() {
+  var elemento = document.getElementById("listaMenu");
+  elemento.style.display = "none";
+  // var elemento = document.getElementById("menu-icon");
+  // elemento.style.content = "\2261";
+}
+
+function mostrarMenu() {
+  var elemento = document.getElementById("listaMenu");
+  elemento.style.display = "initial";
+}
