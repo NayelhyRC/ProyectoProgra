3fb5d2f4298e96fb2cc18d5c42a7ba742927f132
HenrySantamariaC <hsantamariaca@unprg.edu.pe>
2021-05-27 20:00:22 -0500

    Iniciando proyecto php

---
+++ js/panelSlider.js
@@ -0,0 +1,28 @@
+let slider = document.querySelector(".panel-contenedor-slider-contenedor");
+let sliderIndividual = document.querySelectorAll(".panel-contenedor-slider-contenedor-item");
+let contador = 1;
+let intervalo = 3000;
+var width;
+function VerficarWitdh() {
+  width = sliderIndividual[0].clientWidth;
+}
+setInterval(function () {
+  VerficarWitdh();
+}, 100);
+setInterval(function () {
+  slides();
+}, intervalo);
+
+function slides() {
+  slider.style.transform = "translate(" + -width * contador + "px)";
+  slider.style.transition = "transform 3s";
+  contador++;
+
+  if (contador === sliderIndividual.length) {
+    setTimeout(function () {
+      slider.style.transform = "translate(0px)";
+      slider.style.transition = "transform 0s";
+      contador = 1;
+    }, 2790);
+  }
+}
