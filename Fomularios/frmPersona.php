720d39769edd9325fee729302f29d8371bad3f01
Nayelhy <nayelhyrenteriacedano@gmail.com>
2021-06-04 03:57:12 -0500

    Apliqué unos estilitos al frm persona ...

--- Fomularios/frmPersona.php
+++ Fomularios/frmPersona.php
@@ -4,17 +4,42 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../css/estilos.css?v=<?php echo time(); ?>">
+	<link rel="stylesheet" type="text/css" href="../css/responsive.css">
+	<link rel="stylesheet" type="text/css" href="../iconos/style.css">
+	<link rel="stylesheet" type="text/css" href="../css/styleLS.css">
+	<link rel="stylesheet" type="text/css" href="../iconos/LSiconsStyle.css">
+    <link rel="stylesheet" type="text/css" href="../SassNaye/estilo-persona.css">
     <title>DevZone</title>
 </head>
 <body>
-    <h2>Actualizar Usuario</h2>
+    <header class="barra-menu header unselectable">
+		<nav>
+			<input type="checkbox" id="toggle"/>
+			<label class="menu-icon" for="toggle"></label>
+			<ul id="listaMenu">
+				<li class="titulo"><a class="opcionNav" href="#inicio"><i class="ico-home3"></i><span> INICIO</span></a></li>
+				<li class="titulo"><a class="opcionNav" href="#nosotros"><i class="ico-user-tie"></i><span> NOSOTROS</span></a></li>
+				<li class="titulo"><a class="opcionNav" href="#cursos"><i class="ico-books"></i><span> CURSOS</span></a></li>
+				<li class="titulo"><a class="opcionNav" href="#testimonios"><i class="ico-users"></i><span> TESTIMONIOS</span></a></li>
+				<li class="titulo"><a class="opcionNav" href="#contacto"><i class="ico-hangouts"></i><span> CONTACTO</span></a></li>
+				<!-- <li class="titulo"><a class="opcionNav" href="#" onclick="openLoginForm()"><i  class="icon-key"></i><span> INCIAR SESION</span></a></li>			
+				<li class="titulo"><a class="opcionNav" onclick="openSingupForm()"><i class="icon-sign-in"></i><span > REGISTRARSE</span></a></li> -->
+			</ul>
+		</nav>
+	</header>
+    
     <div class="container">
-        <form method="POST" action="funPersona.php">
-            Nombres: <input type="text" placeholder="" name="txtNombres"><br><br>
-            Apellido Paterno: <input type="text" placeholder="" name="txtApellidoPat"><br><br>
-            Apellido Materno: <input type="text" placeholder="" name="txtApellidoMat"><br><br>
-            Fecha de nacimiento: <input id="dtpFecha" type="date" name="dtpFechaNacimiento"><br><br>
-            País: 
+    <h2>Actualizar Usuario</h2>
+        <form id="form-persona" method="POST" action="funPersona.php">
+            <label>Nombres:</label> 
+            <input type="text" placeholder="Ingrese su nombre" name="txtNombres"><br><br>
+            <label>Apellido Paterno: </label>
+            <input type="text" placeholder="Ingrese su apellido paterno" name="txtApellidoPat"><br><br>
+            <label>Apellido Materno: </label>
+            <input type="text" placeholder="Ingrese su apellido materno" name="txtApellidoMat"><br><br>
+            <label>Fecha de nacimiento: </label><input id="dtpFecha" type="date" name="dtpFechaNacimiento"><br><br>
+            <label>País: </label>
             <select name="cboPais">
                 <option value="AF">Afganistán</option>
                 <option value="AL">Albania</option>
@@ -251,7 +276,8 @@
                 <option value="ZM">Zambia</option>
                 <option value="ZW">Zimbabue</option>
             </select>
-            <input type="submit" name="btnEnviar" value="Registrar">
+            <br>
+            <input type="submit" name="btnEnviar" value="Guardar">
         </form>
 
         <script>
@@ -268,5 +294,64 @@
             }
         </script>
     </div>
+
+    <footer class="footer">
+		<div id="contacto" class="contacto">
+			<div class="containter-footer-body child-container" id="footer">
+				<div class="columns column1">
+					<h2>Más sobre nosotros</h2>
+					<p>Somos un grupo de estudiantes lanzando su primera startup.</p>
+				</div>
+				<div class="columns column2">
+					<h2>Nuestras redes sociales</h2>
+					<ul>
+						<li>
+							<a href="https://www.facebook.com/profile" target="_blank">
+								<i class="ico-facebook2"></i> Síguenos en Facebook
+							</a>
+						</li>
+
+						<li>
+							<a href="https://www.facebook.com/profile" target="_blank">
+								<i class="ico-twitter"></i> Síguenos en Twitter
+							</a>
+						</li>
+
+						<li>
+							<a href="https://www.facebook.com/profile" target="_blank">
+								<i class="ico-instagram"></i> Síguenos en Instagram
+							</a>
+						</li>
+
+						<li>
+							<a href="https://www.facebook.com/profile" target="_blank">
+								<i class="ico-youtube"></i> Síguenos en Youtube
+							</a>
+						</li>
+					</ul>
+				</div>
+
+				<div class="columns column3">
+					<h2>Contáctanos</h2>
+					<ul>
+						<li>
+							Mail: <a href="mailto:correo@gmail.com">correo@gmail.com</a>
+						</li>
+					</ul>
+					
+				</div>
+			</div>
+		</div>
+		<div>
+			<div class="container-footer">
+				<p>
+					Copyright &#169; Devzonee 
+					<script type='text/javascript'>
+						var creditsyear = new Date();document.write(creditsyear.getFullYear());
+					</script>. All rights reserved
+				</p>
+			</div>
+		</div>
+  	</footer>
 </body>
 </html>
